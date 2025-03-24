b233eeba5d22313f822d4e06331b87f10e26c7a9
SyedHassanUlHaq <ulhaqhassan1@gmail.com>
2025-03-24 21:24:58 +0500

    configs WMT_quaterly

--- scrapers/Scraper.py
+++ scrapers/Scraper.py
@@ -124,14 +124,14 @@ class Scraper:
                         json.dump(all_events, f, indent=4)
                     print(f"\n✅ Data saved in: {self.output_file}")
                     
-                    # await output_event_JSON_to_file(
-                    #     input_json_file=self.output_file,
-                    #     output_json_file=self.output_json,
-                    #     equity_ticker=self.ticker,
-                    #     geography=self.geography,
-                    #     periodicity=self.periodicity,
-                    #     base_url = self.base_address
-                    #     )
+                    await output_event_JSON_to_file(
+                        input_json_file=self.output_file,
+                        output_json_file=self.output_json,
+                        equity_ticker=self.ticker,
+                        geography=self.geography,
+                        periodicity=self.periodicity,
+                        base_url = self.base_address
+                        )
                 else:
                     print("\n❌ No events found.")
 
