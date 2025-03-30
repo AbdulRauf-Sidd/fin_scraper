0224fc0638cf84d2cf02a4ed7680919faae7c02d
AbdulRauf Siddiqui <abdulrauf_99@outlook.com>
2025-03-30 23:13:40 +0500

    modified SS script to return png instead

--- utils/utils.py
+++ utils/utils.py
@@ -323,7 +323,7 @@ async def capture_full_page_screenshot(context, page=None, url=None):
         logging.info("Taking full page screenshot...")
         await page.screenshot(path=file_path, full_page=True)
         absolute_path = os.path.abspath(file_path)
-        file_type = 'image/png'
+        file_type = 'png'
         logging.info(f"Saved screenshot: {absolute_path}")
         if page is None:
             await page.close()
