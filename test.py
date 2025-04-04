f821f19d69f6d5f3891d8c44bd3c74fdfbebcdec
AbdulRauf Siddiqui <abdulrauf_99@outlook.com>
2025-04-04 14:59:14 +0500

    fixed main

--- test.py
+++ test.py
@@ -55,5 +55,5 @@ async def download_file(url, base_url="https://www.sec.gov", headless=False):
         
         
 asyncio.run(download_file(url=
-"https://www.novonordisk.com/content/dam/Denmark/HQ/investors/irmaterial/annual_report/2014/20140203_Annual%20Report%202013_UK.pdf"
+"https://ir.iff.com/news-releases/news-release-details/iff-and-kemira-form-joint-venture-produce-sustainable-biobased"
 , headless=False, base_url='https://www.reckitt.com'))
